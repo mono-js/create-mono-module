47a5bc7126598991d74dfd708736964462ec6ae2
Sebastien Chopin <sebastien.chopin@neo9.fr>
2017-10-20 15:27:01 +0200

    Update example

--- template/lib/acl.js
+++ template/lib/acl.js
@@ -8,7 +8,11 @@ const { imperium } = require('@terrajs/mono')
 /*
 ** Example:
 
+See https://github.com/terrajs/imperium for documentation
+
 imperium.role('admin', (req) => !!req.session.admin)
-imperium.role('user', (req) => !!req.session.userId)
+imperium.role('user', async (req) => {
+	return { user: req.session.userId }
+})
 
 */
